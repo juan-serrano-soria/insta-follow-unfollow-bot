45c91f059f6c24d62bbc0c614b1f4a2126eafa41
Juan Serrano Soria <juanss1001@gmail.com>
2020-08-08 11:13:06 +0200

    follow.py now reads a file and does the following

--- follow.py
+++ follow.py
@@ -9,5 +9,14 @@ psw = getpass("Instagram password: ")
 session = InstaPy(username=user, password=psw, geckodriver_path='./driver/geckodriver')
 session.login()
 
+# open text file and read it into a list
+f = open("sample_accounts_followers.txt", "r")
+accounts = f.read().splitlines()
+f.close()
+
+# follow these accounts, without ignoring private accounts
+session.set_skip_users(skip_private=True, private_percentage=0)
+follow_by_list(followlist=accounts, times=1, sleep_delay=600, interact=False)
+
 # close session
 session.end()
